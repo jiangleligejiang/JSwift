5c4f5197fb727ed4d702298d2e437f8bc5e1958e
jams <liuqiang04@corp.netease.com>
2019-10-12 17:42:14 +0800

    #划分rxswift目录

--- RxSwiftCode/RxSwiftCode/ViewController.swift
+++ RxSwiftCode/RxSwiftCode/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.simpleTest()
+        //self.simpleTest()
         self.schedulersTest()
     }
 
